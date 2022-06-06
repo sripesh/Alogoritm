f506ec3e121276b9b22b99bdb8645f39e5e06de0
Amarjit Sripesh <thunderworriers@gmail.com>
2022-06-06 17:12:00 +0530

    added Comments

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,40 +1,59 @@
+//Given an already sorted array of positive integers, design an algorithm and implement it using a
+//program to find whether given key element is present in the array or not. Also, find total number
+//of comparisons for each input case. (Time Complexity = O(nlogn), where n is the size of input).
+
+
 #include<stdio.h>
+
+/*********************************
+ ****      BINARY_SEARCH      ****
+ *********************************/
+
 int bs(int ar[],int l,int r,int *lo,int key)
 {
-    int mid=(l+r)/2;
-    *lo=*lo+1;
-    if(l==r)
-    return 0;
-    if(ar[mid]==key)
-    return 1;
-    else if(ar[mid]>key)
-    return bs(ar,l,mid-1,lo,key);
-    else
-    return bs(ar,mid+1,r,lo,key);
+    int mid=l+(r-l)/2;                              //calculate the mid
+    *lo=*lo+1;                                      //increment the count of camparisons
+
+    if(l==r)                                        //if lower limit is equal to upper limit
+    return 0;                                       //return key element is not present(0)
+
+    if(ar[mid]==key)                                //if key element is present in mid index
+    return 1;                                       //return key element is present(1)
+
+    else if(ar[mid]>key)                            //else if key element is present before mid index
+    return bs(ar,l,mid-1,lo,key);                   //recursion for elements before mid index
+
+    else                                            //else if key element is present after mid index
+    return bs(ar,mid+1,r,lo,key);                   //recursion for elements after mid index
 }
+
+/********************************
+ ****          MAIN          ****
+ ********************************/
+
 int main()
 {
     int ca;
-    scanf("%d",ca);
-    while(ca--)
+    scanf("%d",&ca);                                 //input for number of test cases
+    while(ca--)                                     //loop for the number of test cases
     {
-        int num;
-        scanf("%d",num);
+        int num;                                    
+        scanf("%d",&num);                            //input the size of array
         int arr[num];
         for(int i=0;i<num;i++)
         {
-            scanf("%d",arr[i]);
+            scanf("%d",&arr[i]);                     //input the element in the array
         }
         int key;
-        scanf("%d",key);
+        scanf("%d",&key);                            //input the key element to search
         int loop=0;
-        if(bs(arr,0,num-1,&loop,key))
+        if(bs(arr,0,num-1,&loop,key))               //if key element is present in the array
         {
-            printf("Present %d\n",loop);
+            printf("Present %d\n",loop);            //print that the key is PRESENT
         }
-        else
+        else                                        //else if key element is not present in the array
         {
-            printf("Not Present %d\n",loop);
+            printf("Not Present %d\n",loop);        //print that the key is NOT PRESENT
         }
     }
 }
--- LinearSearch.c
+++ LinearSearch.c
@@ -1,26 +1,37 @@
+//Given an array of nonnegative integers, design a linear algorithm and implement it using a
+//program to find whether given key element is present in the array or not. Also, find total number
+//of comparisons for each input case. (Time Complexity = O(n), where n is the size of input)
+
+
 #include<stdio.h>
+
+
+/********************************
+ ****          MAIN          ****
+ ********************************/
+
 int main()
 {
-    int ca;
-    scanf("%d",ca);
-    while(ca--)
+    int ca;                                     
+    scanf("%d",&ca);                                 //input for number of test cases
+    while(ca--)                                     //loop for the number of test cases
     {
-        int num,loop=0,key,i=0;
-        scanf("%d",num);
+        int num,loop=0,key,i=0;                     
+        scanf("%d",&num);                            //input the size of array
         int arr[num];
         for(int i=0;i<num;i++)
-        scanf("%d",arr[i]);
-        scanf("%d",key);
-        for(;i<num;i++)
+        scanf("%d",&arr[i]);                         //input the element in the array
+        scanf("%d",&key);                            //input the key element to search
+        for(;i<num;i++)                             //loop for linear search
         {
-            loop++;
-            if(key==arr[i])
+            loop++;                                 //count the number of comparisons
+            if(key==arr[i])                         //if the key element is present
             {
-                printf("Present %d\n",loop);
-                break;
+                printf("Present %d\n",loop);        //print that the key is PRESENT
+                break;                              //breaks out of the loop
             }
         }
-        if(i==num)
-        printf("Not Present %d\n",loop);
+        if(i==num)                                  //if the 'i' is equal to size of array
+        printf("Not Present %d\n",loop);            //key element is NOT PRESENT
     }
 }
